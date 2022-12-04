424e785773716764ff084ab299dcd428ff9a56ba
mortensenErik <morten47@uwm.edu>
2022-12-04 17:08:01 -0600

    Added post for Login Added post to Home for logging out

--- app/views.py
+++ app/views.py
@@ -1,12 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UserProfile, Course, Section
+from django.contrib.auth import login, logout, authenticate
+
 
 class Home(HttpResponse):
     def get(self, request):
-        return render (request, "Home.html", {})
+        return render(request, "Home.html", {})
+
+    def post(self, request):
+        logout(request)
+        return redirect("Login.html")
+
+
 class Login(HttpResponse):
     def get(self, request):
         return render(request, "Login.html", {})
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return render(request, "Home.html", {})
+        else:
+            return render(request, "Login.html", {})
+
+
 class Profile(HttpResponse):
     def get(self, request):
         return render (request, "Profile.html", {})
@@ -16,9 +35,17 @@ class createCourse(HttpResponse):
 class createSection(HttpResponse):
     def get(self, request):
         return render (request, "createSection.html", {})
+
+class createUser(HttpResponse):
+    def get(selfself, request):
+        return render (request, "createUser.html", {})
 class editCourse(HttpResponse):
     def get(self, request):
         return render (request, "editCourse.html", {})
+
+class editProfile(HttpResponse):
+    def get(self, request):
+        return render (request, "editProfile.html", {})
 class editSection(HttpResponse):
     def get(self, request):
         return render (request, "editSection.html", {})
