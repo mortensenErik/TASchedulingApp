6622b6dd8b429e395c7f06d1aad28d1ba7abb840
ejmon <ejmonka@uwm.edu>
2022-12-04 16:41:46 -0600

    Made files and class skeletons for acceptance tests

---
+++ acceptance tests/test_account_edit.py
@@ -0,0 +1,13 @@
+from django.test import TestCase, Client
+from app.models import UserProfile
+
+
+class testEditAccount(TestCase):
+    def setUp(self):
+        pass
+
+    def testEditNewAccount(self):
+        pass
+
+    def testEditOldAccount(self):
+        pass
---
+++ acceptance tests/test_admin_view_data.py
@@ -0,0 +1,16 @@
+from django.test import TestCase, Client
+from app.models import UserProfile, Course, Section
+
+
+class testAdminViewData(TestCase):
+    def setUp(self):
+        pass
+
+    def testViewUsers(self):
+        pass
+
+    def testViewCourses(self):
+        pass
+
+    def testViewSections(self):
+        pass
