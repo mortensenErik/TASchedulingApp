3987a0f95992b5ae9b265b14886469da9a1b0d81
RamRavipati0406 <ravipati@uwm.edu>
2022-12-04 17:29:06 -0600

    commit things

--- unit_tests/test_section.py
+++ unit_tests/test_section.py
@@ -64,5 +64,16 @@ class CreateSectionTest(TestCase):
                                  id="1253", instructor="human")
         self.tempCourse.save()
 
-    def test_section_things(self):
-        okay = make_section({"id": "idnumber"})
+    def test_is_section_made(self):
+        okay = make_section({"id": "12350"},{"number": 801},{"type": "lab"})
+        self.assertTrue(okay, msg="Error: creating a section for a course that exists")
+        lookfor = list(Section.objects.filter(course_name="compsci",number="351"))
+        self.assertEqual(len(lookfor), 1, msg="Error: not even a single section is created for the course")
+        self.assertEqual(lookfor[0].number,801,"Error: the section that was inteded is not what was created")
+
+
+    def test_data_is_not_right(self):
+        okay =  make_section({"id": 5454 },{"number": 801},{"type": "lab"})
+        self.assertFalse(okay,msg="Error: course was made even with a bad id")
+        lookfor= list(Section.objects.filter(course_name="compsci",number=351))
+        self.assertEqual(len(lookfor),0,msg="Error: a course was made when course doesn't exist")
