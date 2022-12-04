eab236b349d7e17f9791fb663504b913743de4be
Haitam Chouiekh <chouiekh@uwm.edu>
2022-12-04 15:35:02 -0600

    created the skeleton for some views

--- app/views.py
+++ app/views.py
@@ -7,7 +7,32 @@ class Login(View):
     @staticmethod
     def get(request):
         # request.session["username"] = None
-        request.session["current"] = ""
+        # request.session["current"] = ""
         # request.session["user_type"] = None
         # request.session["user_id"] = None
+        return render(request, "Login.html")
+
+
+class Home(View):
+    @staticmethod
+    def get(request):
         return render(request, "Home.html")
+
+
+class CreateUser(View):
+    @staticmethod
+    def get(request):
+        return render(request, "createUser.html")
+
+
+class CreateCourse(View):
+    @staticmethod
+    def get(request):
+        return render(request, "createCourse.html")
+
+
+class CreateSection(View):
+    @staticmethod
+    def get(request):
+        return render(request, "createSection.html")
+
--- project/urls.py
+++ project/urls.py
@@ -5,4 +5,8 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Login.as_view()),
+    path('home/', Home.as_view()),
+    path('new_user/', CreateUser.as_view()),
+    path('new_course/', CreateCourse.as_view()),
+    path('new_section/', CreateSection.as_view()),
 ]
