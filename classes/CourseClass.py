829f529836e5666cea71d2dec7cce4c4eed4e85a
sbrogni <98424655+sbrogni@users.noreply.github.com>
2022-12-05 19:36:22 -0600

    Update CourseClass.py

--- classes/CourseClass.py
+++ classes/CourseClass.py
@@ -1,33 +1,90 @@
-class Course:
+from typing import Union
+from app.models import Course
+
+
+class ErrorString:
 
     def __init__(self, id, name, instructor, number, sections):
-        self.id = id
-        self.number = number
-        self.name = name
-        self.sections = sections
-        self.instructor = instructor
+        def __init__(self, message):
+            self.message = message
+
+        def __str__(self):
+            return self.message
+
+        def __bool__(self):
+            return False
 
 
-    def setInstructor(self, instructor):
-        self.instructor = instructor
 
-    def getInstructor(self):
-        return self.instructor
+def verify_dict(needed: list[(str, type)], toVerify: dict) -> Union[ErrorString, bool]:
+    for i in needed:
+        if not toVerify.get(i[0]):
+            return ErrorString("Error: " + i[0] + " was not provided")
+        if type(toVerify[i[0]]) is not i[1]:
+            return ErrorString("Error: invalid type for " + i[0])
 
-    def setName(self, name):
-        self.name = name
+    return True
 
-    def getName(self):
-        return self.name
 
-    #adds a section
-    def addSection(self):
-        pass
+def make_course(courseinfo: dict) -> Union[ErrorString, bool]:
+    """
+    makes a course. returns an ErrorString, describing the error, if fails to create a course.
+    returns true if successfully created a new course
+    """
+    needed = [("id", str), ("name", str), ("number", int), ("instructor", str)]
+    check = verify_dict(needed, courseinfo)
 
-    #removes a section
-    def removeSection(self):
-        pass
+    if not check:
+        return check
 
-    def getSections(self):
-        return self.sections
+    lookup = Course.objects.filter(number = courseinfo["number"]).exists()
+
+    if lookup:
+        return ErrorString("Error: this course already exists, enter another course number to add")
+    else:
+        Course.objects.create(name = courseinfo["name"], number = courseinfo["number"])
+
+    return True
+
+
+def delete_course(courseinfo: dict) -> Union[ErrorString, bool]:
+    """
+    deletes a course, returns an ErrorString if fails to delete course. return true if successfully
+    deletes a course
+    """
+    needed = [("id", str), ("name", str), ("number", int), ("instructor", str)]
+    check = verify_dict(needed, courseinfo)
+
+    if not check:
+        return check
+
+    lookup = Course.objects.filter(number = courseinfo["number"]).exists()
+
+    if lookup:
+        Course.objects.delete(name = courseinfo["name"], number = courseinfo["number"])
+    else:
+        return ErrorString("Error: this course has not been created, unable to delete")
+
+    return True
+    
+
+def assign_instructor_to_course(courseinfo: dict) -> Union[ErrorString, bool]:
+    """
+    assigns an instructor to a course. if an instructor is already assigned, the new instructor
+    will be added replacing old instructor
+    """
+    needed = [("id", str), ("name", str), ("number", int), ("instructor", str)]
+    check = verify_dict(needed, courseinfo)
+
+    if not check:
+        return check
+
+    lookup = Course.objects.filter(number = courseinfo["number"]).exists()
+
+    if lookup:
+        Course.objects.update(name = courseinfo["name"], number = courseinfo["number"], instructor = courseinfo["instructor"])
+    else:
+        return ErrorString("Error: course has not been created, unable to assign instructor")
+
+    return True
 
