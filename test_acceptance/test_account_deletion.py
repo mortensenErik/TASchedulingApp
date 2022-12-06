1b29b603701c9dc1071e49f2ea51f9f159d12bec
ejmon <ejmonka@uwm.edu>
2022-12-06 15:14:33 -0600

    Wrote tests for account deletion. Added delete profile button to the editProfile template

--- test_acceptance/test_account_deletion.py
+++ test_acceptance/test_account_deletion.py
@@ -7,6 +7,13 @@ class testDeleteAccount(TestCase):
         self.nigel = UserProfile.objects.create(name='Nigel', id='bigboy', password='boogy', email='bigboy@uwm.edu',
                                                 phone='202-555-0196', address='abc123', officeHours="Appointment Only",
                                                 role="admin")
+        self.frank = UserProfile.objects.create(name='Frank Furter', id='frankf', password='hotdog', email='frankf@uwm.edu',
+                                                phone='202-555-0111', address='abc123', officeHours="Appointment Only",
+                                                role="TA")
 
     def testDeleteAccount(self):
-        pass
+        self.monkey.get("/editProfile/", {"name": "Frank Furter", "password": "hotdog", "phone": "202-555-0111",
+                                          "address": "abc123", "officeHours": "Appointment Only", "role": "TA",
+                                          "email": "frankf@uwm.edu"})
+        self.monkey.post("/editProfile/", {"delete": "on"})
+        self.assertEqual(len(UserProfile.objects.filter(id="frankf")), 0, msg="User was not deleted")
