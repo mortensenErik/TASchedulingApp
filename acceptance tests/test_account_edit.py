9ce7644a1ef75d2e86b3b05ec1ec6868130c8d27
mortensenErik <morten47@uwm.edu>
2022-12-07 15:29:28 -0600

    account edit written, may need addition.

--- acceptance tests/test_account_edit.py
+++ acceptance tests/test_account_edit.py
@@ -4,10 +4,15 @@ from app.models import UserProfile
 
 class testEditAccount(TestCase):
     def setUp(self):
-        pass
+        self.monkey = Client()
+        self.paul = UserProfile.objects.create(name='Paul', id='paulie', password='dietcoke', email='paulie@uwm.edu',
+                                               phone='202-555-0199', address='abc129', officeHours="Never!",
+                                               role="Instructor")
 
-    def testEditNewAccount(self):
-        pass
+    def testEditAccount(self):
+        self.monkey.get("/edit_section/", {"email": "paulie@uwm.edu", "name": "Paul", "password": "dietcoke",
+                                           "role": "Instructor", "phone": "yes", "address": "abc129",
+                                           "officeHours": "Never!"})
+        self.monkey.post("/edit_section/", {"officeHours": "Forever!"})
+        self.assertEqual(self.paul.officeHours, "Forever!", msg="Account not updated")
 
-    def testEditOldAccount(self):
-        pass
--- acceptance tests/test_admin_assign_instructor.py
+++ acceptance tests/test_admin_assign_instructor.py
@@ -8,17 +8,18 @@ class testAdminAssignInstructor(TestCase):
         self.nigel = UserProfile.objects.create(name='Nigel', id='bigboy', password='boogy', email='bigboy@uwm.edu',
                                                 phone='202-555-0196', address='abc123', officeHours="Appointment Only",
                                                 role="Instructor")
-        self.Miguel = UserProfile.objects.create(name='Miguel', id='miggy', password='ziggy', email='miggy@uwm.edu',
+        self.miguel = UserProfile.objects.create(name='Miguel', id='miggy', password='ziggy', email='miggy@uwm.edu',
                                                 phone='202-555-0190', address='abc120', officeHours="Always",
                                                 role="TA")
         self.cs361 = Course.objects.create(id='361', name='Intro to Software Engineering', number='CS361',
                                            instructor=None)
+        self.cs361Lec = Section.objects.create(id='401', type='LEC', course=self.cs361, teacher=None)
     def testAssignInstructor(self):
-        self.monkey.get("edit_course/", {"courseName": "Intro to Software Engineering", "courseNumber": "CS361"})
-        self.monkey.post("edit_course/", {"courseInstructor": "bigboy@uwm.edu"})
-        self.assertEqual(self.cs361.instructor, self.nigel, msg="Instructor not assigned to course")
+        self.monkey.get("/edit_section/", {"sectionNumber": "361-401", "teacher": "", "sectionType": "LEC"})
+        self.monkey.post("/edit_section/", {"teacher": "bigboy@uwm.edu"})
+        self.assertEqual(self.cs361Lec.teacher, self.nigel, msg="Instructor was not assigned to course")
 
     def testCourseAssignTa(self):
-        self.monkey.get("edit_course/", {"courseName": "Intro to Software Engineering", "courseNumber": "CS361"})
-        self.monkey.post("edit_course/", {"courseInstructor": "miggy@uwm.edu"})
-        self.assertEqual(self.cs361.instructor, None, msg="User cannot be assigned to course")
+        self.monkey.get("/edit_section/", {"sectionNumber": "361-401", "teacher": "", "sectionType": "LEC"})
+        self.monkey.post("/edit_section/", {"teacher": "miggy@uwm.edu"})
+        self.assertEqual(self.cs361.instructor, None, msg="User cannot be assigned to this section")
