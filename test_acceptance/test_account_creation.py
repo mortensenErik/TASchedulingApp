8198761b55490485bcc0fc17b34e4ad53c5ed66f
ejmon <ejmonka@uwm.edu>
2022-12-10 16:55:43 -0600

    Fixed acceptance tests to work with new database models, variable names, and views. account creation and data viewing tests pass. The others fail

--- test_acceptance/test_account_creation.py
+++ test_acceptance/test_account_creation.py
@@ -6,19 +6,18 @@ class testCreateUser(TestCase):
     def setUp(self):
         self.monkey = Client()
         self.nigel = UserProfile.objects.create(name='Nigel', id='bigboy', password='boogy', email='bigboy@uwm.edu',
-                                                phone='202-555-0196', address='abc123', officeHours="Appointment Only",
-                                                role="admin")
+                                                phone='202-555-0196', address='abc123', role="Admin")
 
     def testCreateNewUser(self):
-        resp = self.monkey.post("/createUser/", {"email": "shaqon@uwm.edu", "name": "Shaquille O'Neal",
-                                                 "password": "basketball", "role": "admin", "phone": "yes",
-                                                 "address": "EMS E250", "officeHours": "M 9:00am-11:30am"})
+        resp = self.monkey.post("/new_user/", {"id": "shaqon", "email": "shaqon@uwm.edu", "name": "Shaquille O'Neal",
+                                               "password": "basketball", "role": "Admin", "phone": "yes",
+                                               "address": "EMS E250"})
         self.assertEqual(len(UserProfile.objects.filter(id="shaqon")), 1, msg="User was not created")
 
     def testCreateOldUser(self):
-        resp = self.monkey.post("/createUser/", {"email": "bigboy@uwm.edu", "name": "Shaquille O'Neal",
-                                                 "password": "basketball", "role": "admin", "phone": "yes",
-                                                 "address": "EMS E250", "officeHours": "M 9:00am-11:30am"})
+        resp = self.monkey.post("/new_user/", {"id": "bigboy", "email": "bigboy@uwm.edu", "name": "Shaquille O'Neal",
+                                               "password": "basketball", "role": "Admin", "phone": "yes",
+                                               "address": "EMS E250"})
         self.assertEqual(len(UserProfile.objects.filter(id="bigboy")), 1, msg="Multiple users with the same id exist")
         self.assertEqual(len(UserProfile.objects.filter(id="bigboy", name="Nigel")), 1,
                          msg="An existing user's data was overwritten")
--- test_acceptance/test_account_deletion.py
+++ test_acceptance/test_account_deletion.py
@@ -5,15 +5,14 @@ class testDeleteAccount(TestCase):
     def setUp(self):
         self.monkey = Client()
         self.nigel = UserProfile.objects.create(name='Nigel', id='bigboy', password='boogy', email='bigboy@uwm.edu',
-                                                phone='202-555-0196', address='abc123', officeHours="Appointment Only",
-                                                role="admin")
-        self.frank = UserProfile.objects.create(name='Frank Furter', id='frankf', password='hotdog', email='frankf@uwm.edu',
-                                                phone='202-555-0111', address='abc123', officeHours="Appointment Only",
+                                                phone='202-555-0196', address='abc123', role="admin")
+        self.frank = UserProfile.objects.create(name='Frank Furter', id='frankf', password='hotdog',
+                                                email='frankf@uwm.edu', phone='202-555-0111', address='abc123',
                                                 role="TA")
 
     def testDeleteAccount(self):
         self.monkey.get("/editProfile/", {"name": "Frank Furter", "password": "hotdog", "phone": "202-555-0111",
-                                          "address": "abc123", "officeHours": "Appointment Only", "role": "TA",
+                                          "address": "abc123", "role": "TA",
                                           "email": "frankf@uwm.edu"})
         self.monkey.post("/editProfile/", {"delete": "on"})
         self.assertEqual(len(UserProfile.objects.filter(id="frankf")), 0, msg="User was not deleted")
