550b9979433dcf642fdad53b45bf1ff5cb168f3e
sbrogni <98424655+sbrogni@users.noreply.github.com>
2022-12-10 15:59:33 -0600

    created create and delete section tests

--- unit_tests/test_section.py
+++ unit_tests/test_section.py
@@ -4,25 +4,27 @@ from app.models import Section
 from app.models import Course
 from app.models import UserProfile
 from classes import SectionClass
-from classes.SectionClass import make_section
+from classes.SectionClass import *
 
 class CreateSectionTest(TestCase):
     def setUp(self):
-        self.rock = UserProfile.objects.create(name="Jay Rock", id = "12", email="fs", phone = 'test', officeHours = 'test', role ='test')
-        self.tempCourse = Course.objects.create(name="compsci", number="351",
-                                 id="1253", instructor = self.rock)
-        self.tempCourse.save()
+        self.rock = UserProfile.objects.create(id="12", name="Jay Rock", password="pw", phone = "test", address="tst" , role ="Instructor", email="test@uwm.edu")
+        self.CS351 = Course.objects.create(CourseId="122", name="Data Structures and Algorithms", number="351", subject="CS")
+        self.CS801 = Section.objects.create(SectionId="1", course=self.CS351, faculty=self.rock, number="801", type="LEC")
 
     def test_is_section_made(self):
-        okay = make_section({{"id": "12350"}, {"number": 801}, {"type": "lab"}})
-        self.assertTrue(okay, msg="Error: creating a section for a course that exists")
-        lookfor = list(Section.objects.filter(course_name="compsci",number="351"))
-        self.assertEqual(len(lookfor), 1, msg="Error: not even a single section is created for the course")
-        self.assertEqual(lookfor[0].number,801,"Error: the section that was inteded is not what was created")
+        test = SectionClass.createSection(SectionId="13", course=self.CS351, faculty=self.rock, number="805", type="LAB")
+        lookup = list(Section.objects.all())
+        self.assertEqual(len(lookup), 2, msg="Error: unable to create section")
+        
+    def test_section_delete(self):
+        test = SectionClass.deleteSection(SectionId="13", course=self.CS351, faculty=self.rock, number="805", type="LAB")
+        lookup = list(Section.objects.all())
+        self.assertEqual(len(lookup), 1, msg="Error: unable to delete section")
 
 
-    def test_data_is_not_right(self):
-        okay =  make_section({{"id": 5454}, {"number": 801}, {"type": "lab"}})
-        self.assertFalse(okay,msg="Error: course was made even with a bad id")
-        lookfor= list(Section.objects.filter(course_name="compsci",number=351))
-        self.assertEqual(len(lookfor),0,msg="Error: a course was made when course doesn't exist")
+    #def test_data_is_not_right(self):
+    #    okay =  make_section({{"id": 5454}, {"number": 801}, {"type": "lab"}})
+    #    self.assertFalse(okay,msg="Error: course was made even with a bad id")
+    #    lookfor= list(Section.objects.filter(course_name="compsci",number=351))
+    #    self.assertEqual(len(lookfor),0,msg="Error: a course was made when course doesn't exist")
