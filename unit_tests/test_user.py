dfe72fbebc031dee33be3d83a1b1a981144347b0
sbrogni <98424655+sbrogni@users.noreply.github.com>
2022-12-10 16:09:25 -0600

    updated create user test, added delete user test

--- unit_tests/test_user.py
+++ unit_tests/test_user.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from app.models import UserProfile
+from classes.UserClass import *
 
 
 class UserUnitTests(TestCase):
@@ -39,7 +40,13 @@ class UserUnitTests(TestCase):
         self.assertEqual(len(admin_list), 0)
         
     def test_create_user(self):
-        test = UserProfile.objects.create(id="tryin", name="Jerry", password="none", phone="414-555-5555", address="test",
+        test = User.createUser(id="tryin", name="Jerry", password="none", phone="414-555-5555", address="test",
                                           role="TA", email="jerr@uwm.edu")
         lookup = list(UserProfile.objects.all())
         self.assertEqual(len(lookup), 5, msg="Error: unable to create user")
+        
+    def test_delete_user(self):
+        test = User.deleteUser(id="tryin", name="Jerry", password="none", phone="414-555-5555", address="test",
+                               role="TA", email="jerr@uwm.edu")
+        lookup = list(UserProfile.objects.all())
+        self.assertEqual(len(lookup), 4, msg="Error: unable to delete user")
