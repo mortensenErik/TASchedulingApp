0ae585a7312fdb14b341c4bbd4738955c545c81d
sbrogni <98424655+sbrogni@users.noreply.github.com>
2022-12-11 15:47:56 -0600

    update create and delete tests

--- unit_tests/test_course.py
+++ unit_tests/test_course.py
@@ -1,36 +1,21 @@
 from django.test import TestCase
-from app.models import Course, UserProfile
-from classes import Course
-from classes.CourseClass import make_course, delete_course, assign_instructor_to_course
+from app.models import Course
+from classes.CourseClass import *
 
 class TestCourse(TestCase):
 
     def setUp(self):
-        self.rock = UserProfile.objects.create(name = "Jay Rock", id = "12", email = "fs", phone = 'test', officeHours = 'test', role = 'test')
-        self.sorenson = UserProfile.objects.create(name="Bob Sorenson", id = "11", email = "fs", phone = "test", officeHours = "test", role = "test")
-        self.CS361 = Course.objects.create(name = "compsci", number = "361", id = "12343", instructor = self.rock["name"])
-        self.CS250 = Course.objects.create(name="compsci", number="250", id="12344", instructor = self.sorenson["name"])
-        self.CS251 = Course.objects.create(name = "compsci", number = "251", id = "12346", instructor = self.sorenson["name"])
+        self.CS361 = Course.objects.create(CourseId="123", name="Introduction to Software Engineering", number="361",
+                                          subject="CS")
+        self.CS250 = Course.objects.create(CourseId="124", name="Introductory Computer Programming", number="250", subject="CS")
+        self.CS251 = Course.objects.create(CourseId="125", name="Intermediate Computer Programming", number="251", subject="CS")
 
     def test_course_made(self):
-        test = make_course({{"id": "12345"}, {"name": "compsci"}, {"number": "351"}, {"instructor": self.sorenson["name"]}})
-        self.assertTrue(test, msg = "Error: trying to create a course that already exists")
-        lookup = list(Course.objects.filter(name = "compsci", number = "351"))
-        self.assertEqual(len(lookup), 1, msg = "Error: course was not added")
-        update = list(Course.objects.filter(name = "compsci"))
-        self.assertEqual(len(update), 4)
+        test = CourseClass.createCourse(CourseId="126", name="Data Structures and Algorithms", number="351", subject="CS")
+        lookup = list(Course.objects.all())
+        self.assertEqual(len(lookup), 4, msg="Error: course was not created")
 
     def test_course_delete(self):
-        test = delete_course({{"id": "12343"}, {"name": "compsci"}, {"number": "361"}, {"instructor": self.rock["name"]}})
-        self.assertTrue(test, msg="Error: unable to delete course")
-        lookup = list(Course.objects.filter(name = "compsci", number = "361", instructor = self.rock["name"]))
-        self.assertEqual(len(lookup), 0, msg = "Error: course was not removed from database")
-        update = list(Course.objects.filter(name = "compsci"))
-        self.assertEqual(len(update), 2)
-
-    def test_course_assign(self):
-        test = assign_instructor_to_course({{"id: 12343"}, {"name": "compsci"}, {"number": "361"}, {"instructor": self.sorenson["name"]}})
-        self.assertTrue(test, msg = "Error, unable to assign instructor to course")
-        lookup = list(Course.objects.filter(id = "12343", name = "compsci", number = "361", instructor = self.rock["name"]))
-        self.assertEqual(len(lookup), 0, msg = "Error: unable to update old instructor with new instructor")
-
+        test = CourseClass.createCourse(CourseId="123", name="Introduction to Software Engineering", number="361", subject="CS")
+        lookup = list(Course.objects.all())
+        self.assertEqual(len(lookup), 2, msg="Error: course was not deleted")
