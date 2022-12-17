dbd1bfff56ea8d9b128e42554c449f6465ef2f6f
Haitam Chouiekh <chouiekh@uwm.edu>
2022-12-17 14:02:08 -0600

    changes

--- classes/UserClass.py
+++ classes/UserClass.py
@@ -14,21 +14,12 @@ class User:
                 return None
         raise TypeError("No parameter provided!")
 
-    # @staticmethod
-    # def getUserById(id):
-    #     if id:
-
 
     @staticmethod
     def createUser(email, name, password, phone, address, role):
         UserProfile.objects.create(email=email, name=name, password=password, phone=phone, address=address,
                                    role=role)
         return True
-        # if User.getUserByEmail(email) is None:
-        #     UserProfile.objects.create(id=id,email=email, name=name, password=password,phone=phone, address=address,role=role)
-        #     return True
-        # else:
-        #     return False
 
     @staticmethod
     def deleteUser(email):
@@ -52,28 +43,6 @@ class User:
                 raise ValueError("This user does not exist!")
         else:
             raise TypeError("Must provide valid email!")
-
-
-
-    # @staticmethod
-    # def editUser(id, email, password, name, phone, address, role):
-    #     if email:
-    #         current_user = User.getUserByEmail(email)
-    #         if current_user:
-    #             current_user.id = id
-    #             current_user.password = password
-    #             current_user.email = email
-    #             current_user.name = name
-    #             current_user.phone = phone
-    #             current_user.address = address
-    #             if role:
-    #                 current_user.role = role
-    #
-    #             current_user.save()
-    #         else:
-    #             raise ValueError("This user does not exist!")
-    #     else:
-    #         raise TypeError("Must provide valid email!")
 
 
 
