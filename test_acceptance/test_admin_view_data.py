cb26af5f9ba471e5cc91742ad828ff1469cdfb15
ejmon <ejmonka@uwm.edu>
2022-12-18 17:01:24 -0600

    Fixed logout button on all html pages. Fixed acceptance tests to work with new models. Admin assigning ta test works with edit sections page, will need to add test for the assigning page connected on the home screen

--- test_acceptance/test_admin_view_data.py
+++ test_acceptance/test_admin_view_data.py
@@ -5,16 +5,16 @@ from app.models import UserProfile, Course, Section
 class TestAdminViewData(TestCase):
     def setUp(self):
         self.monkey = Client()
-        self.nigel = UserProfile.objects.create(name='Nigel', id='bigboy', password='boogy', email='bigboy@uwm.edu',
+        self.nigel = UserProfile.objects.create(name='Nigel', password='boogy', email='bigboy@uwm.edu',
                                                 phone='202-555-0196', address='abc123', role="Instructor")
-        self.frank = UserProfile.objects.create(name='Frank', id='frankf', password='bratwurst', email='frankf@uwm.edu',
+        self.frank = UserProfile.objects.create(name='Frank', password='bratwurst', email='frankf@uwm.edu',
                                                 phone='202-555-0144', address='abc123', role="TA")
-        self.cs361 = Course.objects.create(CourseId='361', name='Intro to Software Engineering', number='361',
+        self.cs361 = Course.objects.create(name='Intro to Software Engineering', number='361',
                                            subject="COMPSCI")
-        self.ee457 = Course.objects.create(CourseId='457', name='Digital Logic Lab', number='457',
+        self.ee457 = Course.objects.create(name='Digital Logic Lab', number='457',
                                            subject="ELECENG")
-        self.ee457Lec = Section.objects.create(SectionId='401', course=self.ee457, faculty=self.nigel, type='LEC')
-        self.cs361Lab = Section.objects.create(SectionId='801', course=self.cs361, faculty=self.frank, type='LAB')
+        self.ee457Lec = Section.objects.create(course=self.ee457, faculty=self.nigel, type='LEC')
+        self.cs361Lab = Section.objects.create(course=self.cs361, faculty=self.frank, type='LAB')
 
     def testViewUsers(self):
         resp = self.monkey.get("/users/")
--- test_acceptance/test_self_edit.py
+++ test_acceptance/test_self_edit.py
@@ -4,17 +4,18 @@ from app.models import UserProfile
 class testEditSelf(TestCase):
     def setUp(self):
         self.monkey = Client()
-        self.nigel = UserProfile.objects.create(name='Nigel', id='bigboy', password='boogy', email='bigboy@uwm.edu',
+        self.nigel = UserProfile.objects.create(name='Nigel', password='boogy', email='bigboy@uwm.edu',
                                                 phone='202-555-0196', address='abc123', role="admin")
-        self.frank = UserProfile.objects.create(name='Frank Furter', id='frankf', password='hotdog',
+        self.frank = UserProfile.objects.create(name='Frank Furter', password='hotdog',
                                                 email='frankf@uwm.edu', phone='202-555-0111', address='abc123',
                                                 role="TA")
-        self.hank = UserProfile.objects.create(name='Hank Henderson', id='hankh', password='password123',
+        self.hank = UserProfile.objects.create(name='Hank Henderson', password='password123',
                                                email='hankh@uwm.edu', phone='212-555-0341', address='abc123',
                                                role="Instructor")
 
     def testEditSelfAdmin(self):
-        self.monkey.post("", {"email": "bigboy@uwm.edu", "password": "boogy"})
+        self.monkey.session["email"] = self.nigel.email
+        self.monkey.session["role"] = self.nigel.role
         self.monkey.post("/edit_profile/", {"email": "bigboy@uwm.edu", "name": "Nigel New", "phone": "7777777777",
                                             "address": "abc123"})
         self.assertEqual(UserProfile.objects.filter(email="bigboy@uwm.edu").first().email, "bigboy@uwm.edu",
@@ -27,7 +28,8 @@ class testEditSelf(TestCase):
                          msg="Phone was not updated")
 
     def testEditSelfTA(self):
-        self.monkey.post("", {"email": "frankf@uwm.edu", "password": "hotdog"})
+        self.monkey.session["email"] = self.frank.email
+        self.monkey.session["role"] = self.frank.role
         self.monkey.post("/edit_profile/", {"email": "frankf@uwm.edu", "name": "Frankenstein", "phone": "8989991234",
                                             "address": "abc123"})
         self.assertEqual(UserProfile.objects.filter(email="frankf@uwm.edu").first().email, "frankf@uwm.edu",
@@ -40,7 +42,8 @@ class testEditSelf(TestCase):
                          msg="Phone was not updated")
 
     def testEditSelfInstructor(self):
-        self.monkey.post("", {"email": "hankh@uwm.edu", "password": "password123"})
+        self.monkey.session["email"] = self.hank.email
+        self.monkey.session["role"] = self.hank.role
         self.monkey.post("/edit_profile/", {"email": "hankh@uwm.edu", "name": "Just Hank", "phone": "4598889898",
                                             "address": "abc123"})
         self.assertEqual(UserProfile.objects.filter(email="hankh@uwm.edu").first().email, "hankh@uwm.edu",
