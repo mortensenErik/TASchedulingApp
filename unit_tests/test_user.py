00deb78d20fed23c564f0011515264835fde279d
mortensenErik <morten47@uwm.edu>
2022-12-21 20:24:30 -0600

    Removed IDs and added Addresses for test_user

--- unit_tests/test_user.py
+++ unit_tests/test_user.py
@@ -1,20 +1,19 @@
 from django.test import TestCase
-
 from app.models import UserProfile
 from classes.UserClass import *
 
 
 class UserUnitTests(TestCase):
     def setUp(self):
-        self.nigel = UserProfile.objects.create(name='Nigel', id='bigboy', password='boogy', email='bigboy@uwm.edu',
+        self.nigel = UserProfile.objects.create(name='Nigel', address='123 maple', password='boogy', email='bigboy@uwm.edu',
                                           phone='202-555-0196', role="INSTRUCTOR")
-        self.jacey = UserProfile.objects.create(name='Jacey', id='jacey', password='lmao', email='jacey@uwm.edu',
+        self.jacey = UserProfile.objects.create(name='Jacey', address='124 maple', password='lmao', email='jacey@uwm.edu',
                                           phone='202-555-0168', role="TA")
-        self.anna = UserProfile.objects.create(name='Annabelle', id='lechonk', password='hiyah', email='lechonk@uwm.edu',
+        self.anna = UserProfile.objects.create(name='Annabelle', address='134 maple', password='hiyah', email='lechonk@uwm.edu',
                                          phone='202-555-0164', role="INSTRUCTOR")
-        self.luke = UserProfile.objects.create(name='Luke', id='bingbong', password='kiki', email='bingbong@uwm.edu',
+        self.luke = UserProfile.objects.create(name='Luke', address='123 mople', password='kiki', email='bingbong@uwm.edu',
                                          phone='202-555-0157', role="ADMIN")
-        self.edytha = UserProfile.objects.create(name='Edytha', id='edytha', password='oof', email='edytha@uwm.edu',
+        self.edytha = UserProfile.objects.create(name='Edytha', address='134 mople', password='oof', email='edytha@uwm.edu',
                                            phone='202-555-0182', role="TA")
 
     def test_list_user_email(self):
@@ -45,7 +44,7 @@ class UserUnitTests(TestCase):
         self.assertEqual(len(admin_list), 0)
         
     def test_create_user(self):
-        test = User.createUser(id="lks", name="Jerry", password="none", phone="414-555-5555", address="test", role="TA",
+        test = User.createUser(name="Jerry", password="none", phone="414-555-5555", address="test", role="TA",
                                email="jerr@uwm.edu")
         lookup = UserProfile.objects.all()
         self.assertEqual(len(lookup), 6, msg="Error: unable to delete user")
