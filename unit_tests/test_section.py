73e05e1f31e00160448665aa5b92495b5dbcab34
RamRavipati0406 <ravipati@uwm.edu>
2022-12-22 05:21:46 -0600

    edited the tests for the sections stuff based on new models new

--- unit_tests/test_section.py
+++ unit_tests/test_section.py
@@ -13,16 +13,21 @@ from classes.UserClass import User
 
 class CreateSectionTest(TestCase):
     def setUp(self):
-        self.rock = UserProfile.objects.create( name="Jay Rock", password="pw", phone="414-555-5556", address="test",
-                                              role="Instructor", email="rock@uwm.edu")
-        self.sorenson = UserProfile.objects.create( name="Bob Sorenson", password="ps", phone="414-555-5557",
+        self.rock = UserProfile.objects.create(id="1", name="Jay Rock", password="pw", phone="414-555-5556",
+                                               address="test",
+                                               role="Instructor", email="rock@uwm.edu")
+        self.sorenson = UserProfile.objects.create(id="21", name="Bob Sorenson", password="ps", phone="414-555-5557",
                                                    address="test1", role="TA", email="sorenson@uwm.edu")
-        self.CS250 = Course.objects.create( name="Introductory Computer Programming", number="250", subject="CS")
-        self.CS400 = Section.objects.create( course=self.CS250, faculty=self.rock, number="400", type="LEC")
-        self.CS805 = Section.objects.create( course=self.CS250, faculty=self.sorenson, number="805", type="LAB")
+        self.CS250 = Course.objects.create(CourseId="123", name="Introductory Computer Programming", number="250",
+                                           subject="CS")
+        self.CS400 = Section.objects.create(SectionId="12", course=self.CS250, faculty=self.rock, number="400",
+                                            type="LEC")
+        self.CS805 = Section.objects.create(SectionId="14", course=self.CS250, faculty=self.sorenson, number="805",
+                                            type="LAB")
 
     def test_is_section_made(self):
-        test = SectionClass.createSection(SectionId="13", course=self.CS250, faculty=self.sorenson, number="803", type="LAB")
+        test = SectionClass.createSection(SectionId="13", course=self.CS250, faculty=self.sorenson, number="803",
+                                          type="LAB")
         lookup = list(Section.objects.all())
         self.assertEqual(len(lookup), 3, msg="Error: unable to create section")
 
@@ -30,14 +35,16 @@ class CreateSectionTest(TestCase):
         test = SectionClass.deleteSection(SectionId="14")
         lookup = list(Section.objects.all())
         self.assertEqual(len(lookup), 1, msg="Error: unable to delete section")
-        
+
     def test_same_sectionId(self):
-        test = SectionClass.createSection( course=self.CS250, faculty=self.rock, number="809", type="LAB")
+        test = SectionClass.createSection(SectionId="14", course=self.CS250, faculty=self.rock, number="809",
+                                          type="LAB")
         lookup = list(Section.objects.all())
         self.assertEqual(len(lookup), 2, msg="Error: unable to create same sectionId")
-        
+
     def test_same_number(self):
-        test = SectionClass.createSection( course=self.CS250, faculty=self.sorenson, number="805", type="LAB")
+        test = SectionClass.createSection(SectionId="13", course=self.CS250, faculty=self.sorenson, number="805",
+                                          type="LAB")
         lookup = list(Section.objects.all())
         self.assertEqual(len(lookup), 2, msg="Error: unable to create same number section")
 
