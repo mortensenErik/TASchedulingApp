f05ea75efa071a20796f5a499198e15821d59a84
RamRavipati0406 <ravipati@uwm.edu>
2022-12-22 05:55:30 -0600

    edited the tests for the sections stuff based on new models with the create section..

--- test_acceptance/test_delete_course.py
+++ test_acceptance/test_delete_course.py
@@ -7,16 +7,16 @@ from app.models import Section
 class testDeleteSection(TestCase):
     def setUp(self):
         self.thingy = Client()
-        tempCourseforsec = Course.objects.create(CourseId="12345", name="data structures", number=351,
+        tempCourseforsec = Course.objects.create( name="data structures", number=351,
                                                  subject="computer science")
-        tempCourseforsec_lab = Section.objects.create(SectionId="123", course=tempCourseforsec, number=801, type="LAB")
-        tempCourseforsec_sec = Section.objects.create(SectionId="124", course=tempCourseforsec, number=401, type="LEC")
-        tempCourseforsec1 = Course.objects.create(CourseId="12346", name="system programimg", number=337,
+        tempCourseforsec_lab = Section.objects.create( course=tempCourseforsec, number=801, type="LAB")
+        tempCourseforsec_sec = Section.objects.create(course=tempCourseforsec, number=401, type="LEC")
+        tempCourseforsec1 = Course.objects.create( name="system programimg", number=337,
                                                   subject="Computer Science")
-        tempCourseforsec1_sec = Section.objects.create(SectionId="125", course=tempCourseforsec1, number=407,
+        tempCourseforsec1_sec = Section.objects.create( course=tempCourseforsec1, number=407,
                                                        type="LEC")
-        tempCourseforsec1_lab = Section.objects.create(SectionId="126", course=tempCourseforsec1, number=409,
+        tempCourseforsec1_lab = Section.objects.create( course=tempCourseforsec1, number=409,
                                                        type="LAB")
 
     def testDeleteSections(self):
-        resp = self.thingy.post("delete_section")
+        self.thingy.session("number") = self.Course
